852e608dca7c41d9f8b923e56187af49e61345fc
Brock Herion <brockheriondev@gmail.com>
2025-07-05 10:01:14 -0500

    created express middleware

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const BASE_URL = "https://api.queuebase.com/v1";
 
-export const VERSION = "0.0.1";
+export const VERSION = "0.0.9";
 
 export const DEFAULT_HEADERS = {
   "Content-Type": "application/json",
